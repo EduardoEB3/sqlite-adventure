613501e346de0d2f238819413566fe1de12a6a4d
EduardoEB3 <alu0101230382@ull.edu.es>
2023-12-29 19:04:36 +0000

    ci: :green_heart: Updating configuration for the pipeline

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -6,11 +6,3 @@ npm run build
 # Copiar archivos a la carpeta de implementación en GitHub Pages
 cp -r static/openapi/* docs/
 cp README.md docs/
-
-# Confirmar y empujar cambios al repositorio
-git config --local user.email "actions@github.com"
-git config --local user.name "GitHub Actions"
-
-git add .
-git commit -m "Deploy to GitHub Pages"
-git push origin gh-pages
